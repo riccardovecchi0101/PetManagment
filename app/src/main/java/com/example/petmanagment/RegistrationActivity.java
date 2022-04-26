abda5f58f13b34a421acb7cdd2ac662c04a07aeb
filippobologna <filippo@fam.bologna.it>
2022-04-26 16:06:46 +0200

    prove di firebase (roba che non funziona)

--- app/src/main/java/com/example/petmanagment/RegistrationActivity.java
+++ app/src/main/java/com/example/petmanagment/RegistrationActivity.java
@@ -2,12 +2,22 @@ package com.example.petmanagment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private DocumentReference DocRef= FirebaseFirestore.getInstance().document("Users/credenziali");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +26,20 @@ public class RegistrationActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.password);
         MaterialButton registerBtn = (MaterialButton) findViewById(R.id.loginBtn);
         registerBtn.setOnClickListener(view -> {
-            Credenziali crHelper = new Credenziali(RegistrationActivity.this);
-            try {
-                crHelper.addUser(username.getText().toString().trim(), password.getText().toString().trim());
+            Map<String,String> users=new HashMap<String,String >();
+            users.put(username.getText().toString(),password.getText().toString());
+            DocRef.set(users).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d("ok","ok");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("no","nooo",e);
+                }
+            });
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-            startActivity(intent);
         });
     }
 }
