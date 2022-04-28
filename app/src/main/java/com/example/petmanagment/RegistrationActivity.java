0ad9a9756cb270948ad073845f4be438b9b26c6a
filippobologna <filippo@fam.bologna.it>
2022-04-28 13:01:03 +0200

    aggiunta progress bar

--- app/src/main/java/com/example/petmanagment/RegistrationActivity.java
+++ app/src/main/java/com/example/petmanagment/RegistrationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,10 +28,16 @@ import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private int CurrentProgress = 0;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedIstanceSate) {
         super.onCreate(savedIstanceSate);
         setContentView(R.layout.activity_registration);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
         TextView email = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
         MaterialButton register = (MaterialButton) findViewById(R.id.loginBtn);
@@ -38,42 +45,47 @@ public class RegistrationActivity extends AppCompatActivity {
         register.setOnClickListener(view -> {
             String em = email.getText().toString();
             String pw = password.getText().toString();
-            if(em.isEmpty())
-            {
+            if (em.isEmpty()) {
                 email.setError("email is required");
                 email.requestFocus();
                 return;
-            }
-            else if(!Patterns.EMAIL_ADDRESS.matcher(em).matches())
-            {
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(em).matches()) {
                 email.setError("Insert a valid email");
                 email.requestFocus();
                 return;
             }
-            if(pw.isEmpty())
-            {
+            if (pw.isEmpty()) {
                 password.setError("password is required");
                 password.requestFocus();
                 return;
             }
-            mAuth.createUserWithEmailAndPassword(em, pw)
-                 .addOnCompleteListener(task -> {
-                     if(task.isSuccessful()){
-                         User user = new User(em, pw);
-                         FirebaseDatabase.getInstance().getReference("users")
-                                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                 .setValue(user).addOnCompleteListener(task1 -> {
-                                     if(task.isSuccessful()){
-                                         Toast.makeText(RegistrationActivity.this, "User has been registered succesfuly", Toast.LENGTH_LONG).show();
-                                         Intent returnback = new Intent(getApplicationContext(), MainActivity.class);
-                                         startActivity(returnback);
-                                     }
-                                     else
-                                         Toast.makeText(RegistrationActivity.this, "Error occurred", Toast.LENGTH_LONG).show();
 
-                                 });
-                     }
-                 });
+            mAuth.createUserWithEmailAndPassword(em, pw)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            User user = new User(em, pw);
+                            progressBar.setVisibility(View.VISIBLE);
+
+                            FirebaseDatabase.getInstance().getReference("users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(task1 -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegistrationActivity.this, "User has been registered succesfuly", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.VISIBLE);
+                                    Intent returnback = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(returnback);
+                                } else {
+                                    Toast.makeText(RegistrationActivity.this, "Failed to register the user", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                }
+
+
+                            });
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "failed to register the user", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
         });
     }
 }
