f31116ecd992f5361f60e557509a87231f91a404
filippobologna <filippo@fam.bologna.it>
2022-05-18 17:49:25 +0200

    recycler view

---
+++ app/src/main/java/com/example/petmanagment/ui/Customers/ListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.petmanagment.ui.Customers;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.petmanagment.R;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+
+    String[] list;
+
+    public ListAdapter(String[] list){
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.customers_list_adapter, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.textView.setText(list[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.length;
+    }
+    //creo un adapter per la recycle view
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
