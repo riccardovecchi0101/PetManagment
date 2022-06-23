d006f0e38ace8a59d205c2773eb7fb0e526394a4
filippobologna <filippo@fam.bologna.it>
2022-06-23 18:32:39 +0200

    push mistica

---
+++ app/src/main/java/com/example/petmanagment/ui/Customers/Pet/PetActivity.java
@@ -0,0 +1,16 @@
+package com.example.petmanagment.ui.Customers.Pet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petmanagment.R;
+
+public class PetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+    }
+}
